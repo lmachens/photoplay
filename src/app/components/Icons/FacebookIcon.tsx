4a5373d0e5e71e27e60cc0f9bda862218c176d03
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-05-26 16:25:50 +0200

    :sparkles: Add social icons

---
+++ src/app/components/Icons/FacebookIcon.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default function FacebookIcon(): JSX.Element {
+  return (
+    <svg
+      id="Facebook_button"
+      data-name="Facebook button"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width="43.812"
+      height="43.812"
+      viewBox="0 0 43.812 43.812"
+    >
+      <defs>
+        <linearGradient
+          id="linear-gradient"
+          x1="0.5"
+          x2="0.5"
+          y2="1"
+          gradientUnits="objectBoundingBox"
+        >
+          <stop offset="0" stop-color="#ffbb3b" />
+          <stop offset="1" stop-color="#ffa90a" />
+        </linearGradient>
+      </defs>
+      <g id="Group_5" data-name="Group 5">
+        <path
+          id="Path_18"
+          data-name="Path 18"
+          d="M21.906,0A21.906,21.906,0,1,0,43.812,21.906,21.931,21.931,0,0,0,21.906,0Zm5.448,22.677H23.79v12.7H18.509v-12.7H16v-4.49h2.51v-2.9c0-2.08.988-5.33,5.33-5.33l3.913.015v4.358h-2.84a1.075,1.075,0,0,0-1.12,1.223v2.638h4.024Z"
+          fill="url(#linear-gradient)"
+        />
+      </g>
+    </svg>
+  );
+}
---
+++ src/app/components/Icons/GoogleIcon.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default function GoogleIcon(): JSX.Element {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width="43.808"
+      height="43.808"
+      viewBox="0 0 43.808 43.808"
+    >
+      <defs>
+        <linearGradient
+          id="linear-gradient"
+          x1="0.5"
+          x2="0.5"
+          y2="1"
+          gradientUnits="objectBoundingBox"
+        >
+          <stop offset="0" stop-color="#ffbb3b" />
+          <stop offset="1" stop-color="#ffa90a" />
+        </linearGradient>
+      </defs>
+      <path
+        id="Google_Logo"
+        data-name="Google Logo"
+        d="M21.9,43.808A21.91,21.91,0,0,1,13.378,1.721,21.91,21.91,0,0,1,30.43,42.087,21.768,21.768,0,0,1,21.9,43.808Zm-.055-34.5A12.538,12.538,0,1,0,34.387,21.849V19.342H21.849v5.015h7.094a7.523,7.523,0,1,1-7.094-10.03,7.411,7.411,0,0,1,4.873,1.816l3.295-3.782A12.432,12.432,0,0,0,21.849,9.311Z"
+        fill="url(#linear-gradient)"
+      />
+    </svg>
+  );
+}
