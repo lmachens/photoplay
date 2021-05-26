803d7ed5be84c6caca2f86b56920113b5e088aec
infoucarsinan <infoucarsinan@gmail.com>
2021-05-26 12:02:31 +0200

    Add Card component

---
+++ src/app/components/Card/Card.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from 'react';
+import classes from './Card.module.css';
+
+type CardProps = {
+  img: string;
+  artist: string;
+};
+
+function Card({ img, artist }: CardProps): JSX.Element {
+  return (
+    <div className={classes.artistCard}>
+      <img className={classes.artistImage} src={img} alt="" />
+      <h3 className={classes.artistName}>{artist}</h3>
+    </div>
+  );
+}
+
+export default Card;
