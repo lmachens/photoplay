7d38f305c63ab886056106e412b3228417e0ed2b
fgottes <f.gottesmann@outlook.com>
2021-05-26 14:35:24 +0200

    ♻️ Rename img to imgSrc and artist to artistName

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import classes from './Card.module.css';
 
 type CardProps = {
-  img: string;
-  artist: string;
+  imgSrc: string;
+  artistName: string;
 };
 
-function Card({ img, artist }: CardProps): JSX.Element {
+function Card({ imgSrc, artistName }: CardProps): JSX.Element {
   return (
     <div className={classes.card__container}>
-      <img className={classes.card__image} src={img} alt="" />
-      <h3 className={classes.card__name}>{artist}</h3>
+      <img className={classes.card__image} src={imgSrc} alt="" />
+      <h3 className={classes.card__name}>{artistName}</h3>
     </div>
   );
 }
