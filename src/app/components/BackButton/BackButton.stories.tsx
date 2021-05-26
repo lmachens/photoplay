2903e310697021ffe72cd5706e3bf189d6b99dc7
lajanzen <lara-janzen@hotmail.com>
2021-05-26 17:22:07 +0200

    Add Back button

---
+++ src/app/components/BackButton/BackButton.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BackButton from './BackButton';
+
+export default {
+  title: 'Component/BackButton',
+  component: BackButton,
+};
+
+export const Back = (): JSX.Element => (
+  <BackButton>
+    <span>&lt;</span>Back
+  </BackButton>
+);
