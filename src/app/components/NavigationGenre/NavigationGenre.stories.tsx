3ab8a794a586f52644eeb19dbae9997169c49a74
rabram1 <romanabram@web.de>
2021-05-27 13:41:41 +0200

    ✨ Refactor CSS and add two more stories

--- src/app/components/NavigationGenre/NavigationGenre.stories.tsx
+++ src/app/components/NavigationGenre/NavigationGenre.stories.tsx
@@ -11,3 +11,15 @@ const categoriesArray = ['Movie', 'Adventure', 'Comedy', 'Family'];
 export const showGenre = (): JSX.Element => (
   <NavigationGenre categories={categoriesArray} />
 );
+
+const categoriesArray2 = ['Trailer', 'Series', 'Funny', 'Families'];
+
+export const showGenre2 = (): JSX.Element => (
+  <NavigationGenre categories={categoriesArray2} />
+);
+
+const categoriesArray3 = ['Hallo', 'Series', 'Movies', 'Array'];
+
+export const showGenre3 = (): JSX.Element => (
+  <NavigationGenre categories={categoriesArray3} />
+);
--- src/app/components/NavigationGenre/NavigationGenre.tsx
+++ src/app/components/NavigationGenre/NavigationGenre.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './NavigationGenre.module.css';
 
 type InputProps = {
@@ -6,17 +6,11 @@ type InputProps = {
 };
 
 function NavigationGenre({ categories }: InputProps): JSX.Element {
-  const [clickedCategorie, setClickedCategorie] = useState<string>('');
   return (
     <div className={styles.container}>
-      {console.log(clickedCategorie)}
-      {categories.map((categorie) => (
-        <a
-          onClick={() => setClickedCategorie(categorie)}
-          key={categorie}
-          className={`${categorie === clickedCategorie && styles.highlight}`}
-        >
-          {categorie}
+      {categories.map((category) => (
+        <a href="#" key={category} className={styles.navLink}>
+          {category}
         </a>
       ))}
     </div>
