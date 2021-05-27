28976685afa001e6f350d8ff6d035f63a0039984
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-05-27 15:24:31 +0200

    ✨ Add 2nd story

--- src/app/components/SocialLogin/SocialLogin.stories.tsx
+++ src/app/components/SocialLogin/SocialLogin.stories.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import FacebookIcon from '../Icons/FacebookIcon';
+import GoogleIcon from '../Icons/GoogleIcon';
 import SocialLogin from './SocialLogin';
 
 export default {
-  title: 'Component/SocialLogin',
   component: SocialLogin,
 };
 
-export const SocialLogins = (): JSX.Element => (
+export const Facebook = (): JSX.Element => (
   <SocialLogin icon={<FacebookIcon />} link={'URL'} />
+);
+
+export const Google = (): JSX.Element => (
+  <SocialLogin icon={<GoogleIcon />} link={'URL'} />
 );
