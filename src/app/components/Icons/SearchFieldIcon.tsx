efd46e630a882a6ee14a179e9ea5def5a287f1ab
phlgr <philipp@gartz.dev>
2021-05-27 13:58:24 +0200

    🐛 Rename function in SearchFieldIcon

--- src/app/components/Icons/SearchFieldIcon.tsx
+++ src/app/components/Icons/SearchFieldIcon.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function SearchIcon(props: React.SVGProps<SVGSVGElement>): JSX.Element {
+function SearchFieldIcon(props: React.SVGProps<SVGSVGElement>): JSX.Element {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -17,4 +17,4 @@ function SearchIcon(props: React.SVGProps<SVGSVGElement>): JSX.Element {
   );
 }
 
-export default SearchIcon;
+export default SearchFieldIcon;
