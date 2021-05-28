22f3f1b44edef68ace6ae17af1c4cfa301c30cee
lajanzen <lara-janzen@hotmail.com>
2021-05-28 11:18:42 +0200

    Rename watchlist to movieCard

--- src/app/pages/Homescreen/Homescreen.tsx
+++ src/app/pages/Homescreen/Homescreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavigationGenre from '../../components/NavigationGenre/NavigationGenre';
 import Rating from '../../components/Rating/Rating';
+import MovieCard from '../../components/MovieCard/MovieCard';
 import styles from './Homescreen.module.css';
 
 type HomescreenProps = {
@@ -17,6 +18,13 @@ function Homescreen({ imgSrc }: HomescreenProps): JSX.Element {
         <Rating value={4}></Rating>
         <div className={styles.navigationGenre}>
           <NavigationGenre categories={categoriesArray}></NavigationGenre>
+        </div>
+        <div className={styles.watchlist}>
+          <h2>Watching</h2>
+          <MovieCard imgSrc="/narcos.png"></MovieCard>
+          <MovieCard imgSrc="/deadpool.png"></MovieCard>
+          <MovieCard imgSrc="/annabelle.png"></MovieCard>
+          <MovieCard imgSrc="/toystory.png"></MovieCard>
         </div>
       </main>
     </div>
