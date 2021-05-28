051e4f4a97bf3bf85685900808063258b825ddde
fgottes <f.gottesmann@outlook.com>
2021-05-28 11:29:03 +0200

    ✨ Add register form structure, svg and buttons

---
+++ src/app/Page/Register.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Register from './Register';
+
+export default {
+  title: 'Page/Register',
+  component: Register,
+};
+
+export const Registration = (): JSX.Element => <Register />;
