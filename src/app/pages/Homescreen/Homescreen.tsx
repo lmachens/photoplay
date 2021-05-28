0315830d3eab610f1da5b72108b3a4a6f3752baa
lajanzen <lara-janzen@hotmail.com>
2021-05-28 12:04:15 +0200

    Add navBar

--- src/app/pages/Homescreen/Homescreen.tsx
+++ src/app/pages/Homescreen/Homescreen.tsx
@@ -3,6 +3,11 @@ import NavigationGenre from '../../components/NavigationGenre/NavigationGenre';
 import Rating from '../../components/Rating/Rating';
 import MovieCard from '../../components/MovieCard/MovieCard';
 import styles from './Homescreen.module.css';
+import NavBarLink from '../../components/NavBarLink/NavBarLink';
+import HomeIcon from '../../components/Icons/HomeIcon';
+import SearchIcon from '../../components/Icons/SearchIcon';
+import DownloadIcon from '../../components/Icons/DownloadIcon';
+import ProfileIcon from '../../components/Icons/ProfileIcon';
 
 type HomescreenProps = {
   imgSrc: string;
@@ -24,6 +29,12 @@ function Homescreen({ imgSrc }: HomescreenProps): JSX.Element {
         <MovieCard imgSrc="/deadpool.png"></MovieCard>
         <MovieCard imgSrc="/annabelle.png"></MovieCard>
         <MovieCard imgSrc="/toystory.png"></MovieCard>
+      </div>
+      <div className={styles.navBar}>
+        <NavBarLink icon={<HomeIcon />} text="Home" />
+        <NavBarLink icon={<SearchIcon />} text="Search" />
+        <NavBarLink icon={<DownloadIcon />} text="Download" />
+        <NavBarLink icon={<ProfileIcon />} text="Profile" />
       </div>
     </main>
   );
