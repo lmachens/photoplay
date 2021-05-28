a6332a6e3bc58417d37aac0363453c427832673a
lajanzen <lara-janzen@hotmail.com>
2021-05-28 10:20:59 +0200

    Add navigation genre

--- src/app/pages/Homescreen/Homescreen.tsx
+++ src/app/pages/Homescreen/Homescreen.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import NavigationGenre from '../../components/NavigationGenre/NavigationGenre';
 import Rating from '../../components/Rating/Rating';
 import styles from './Homescreen.module.css';
 
@@ -6,12 +7,17 @@ type HomescreenProps = {
   imgSrc: string;
 };
 
+const categoriesArray = ['Movie', 'Adventure', 'Comedy', 'Family'];
+
 function Homescreen({ imgSrc }: HomescreenProps): JSX.Element {
   return (
     <div className={styles.container}>
       <main>
         <img className={styles.poster} src={imgSrc} />
         <Rating value={4}></Rating>
+        <div className={styles.navigationGenre}>
+          <NavigationGenre categories={categoriesArray}></NavigationGenre>
+        </div>
       </main>
     </div>
   );
