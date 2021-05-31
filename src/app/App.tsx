a7d4ceafd8cec71108b55183b1d10553bdfa35bc
phlgr <philipp@gartz.dev>
2021-05-31 18:23:29 +0200

    ✨ Create Route from Array

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, RouteProps } from 'react-router-dom';
+import RegisterForm from './Page/Register';
+import Cast from './pages/Cast/Cast';
+import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
+import Homescreen from './pages/Homescreen/Homescreen';
+import Login from './pages/Login/Login';
 import MovieDetails from './pages/MovieDetails/MovieDetails';
+import TVShowDetails from './pages/TVShowDetails/TVShowDetails';
+
+type CustomRouteProps = RouteProps & {
+  Component: () => JSX.Element;
+  path: string;
+};
+
+const routes: CustomRouteProps[] = [
+  { path: '/', Component: Homescreen, exact: true },
+  { path: '/movie/:name', Component: MovieDetails },
+  { path: '/forgotpassword', Component: ForgotPassword },
+  { path: '/login', Component: Login },
+  { path: '/register', Component: RegisterForm },
+  { path: '/cast/:name', Component: Cast },
+  { path: '/show/:name', Component: TVShowDetails },
+];
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/">
-          <p>Home</p>
-        </Route>
-        <Route path="/movie/:name">
-          <MovieDetails />
-        </Route>
+        {routes.map(({ Component, ...routeProps }) => (
+          <Route key={routeProps.path} {...routeProps}>
+            <Component />
+          </Route>
+        ))}
       </Switch>
     </BrowserRouter>
   );
