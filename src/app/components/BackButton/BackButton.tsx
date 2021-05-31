6026c2e03d3259bf0e4f6c215df0fa852e23106f
phlgr <philipp@gartz.dev>
2021-05-31 16:11:16 +0200

    ✨ Add history to BackButton

--- src/app/components/BackButton/BackButton.tsx
+++ src/app/components/BackButton/BackButton.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './BackButton.module.css';
+import { useHistory } from 'react-router-dom';
 
 function BackButton(): JSX.Element {
+  const history = useHistory();
+
   return (
-    <a className={styles.backButton} href="#">
+    <button className={styles.backButton} onClick={() => history.goBack()}>
       <span className={styles.backArrow}>&lt;</span>
       Back
-    </a>
+    </button>
   );
 }
 
