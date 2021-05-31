1bdcef175f8777999b508f047c7d6165bec343cc
phlgr <philipp@gartz.dev>
2021-05-31 15:08:36 +0200

    ✨ Add new movie route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import MovieDetails from './pages/MovieDetails/MovieDetails';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/help">
-          <p>Help</p>
-        </Route>
-        <Route path="/">
+        <Route exact path="/">
           <p>Home</p>
+        </Route>
+        <Route path="/movie/:name">
+          <MovieDetails />
         </Route>
       </Switch>
     </BrowserRouter>
