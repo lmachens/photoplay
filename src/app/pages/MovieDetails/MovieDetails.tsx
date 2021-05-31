1cca404c6d35ba1db810ff245f55f7ab5127bdf1
daniel.ebert <daniel.ebert@web.de>
2021-05-31 11:28:24 +0200

    :recycle: integrate NAvBar as a component

--- src/app/pages/MovieDetails/MovieDetails.tsx
+++ src/app/pages/MovieDetails/MovieDetails.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import BackButton from '../../components/BackButton/BackButton';
 import Button from '../../components/Button/Button';
-import DownloadIcon from '../../components/Icons/DownloadIcon';
-import HomeIcon from '../../components/Icons/HomeIcon';
-import ProfileIcon from '../../components/Icons/ProfileIcon';
-import SearchIcon from '../../components/Icons/SearchIcon';
-// import michaelPenaImage from '../../components/ArtistCard/ArtistCard';
-import NavBarLink from '../../components/NavBarLink/NavBarLink';
 import ArtistCard from '../../components/ArtistCard/ArtistCard';
 import styles from './MovieDetails.module.css';
 import Rating from '../../components/Rating/Rating';
 import NavigationGenre from '../../components/NavigationGenre/NavigationGenre';
+import NavBar from '../../components/NavBar/NavBar';
 
 const categoriesArray = ['Movie', 'Adventure', 'Comedy', 'Family'];
 
@@ -55,12 +50,7 @@ function MovieDetails(): JSX.Element {
         </div>
       </main>
       <footer className={styles.footer}>
-        <div className={styles.navBar}>
-          <NavBarLink icon={<HomeIcon />} text="Home" />
-          <NavBarLink icon={<SearchIcon />} text="Search" />
-          <NavBarLink icon={<DownloadIcon />} text="Download" />
-          <NavBarLink icon={<ProfileIcon />} text="Profile" />
-        </div>
+        <NavBar />
       </footer>
     </div>
   );
