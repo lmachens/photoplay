bd617667e0027d3571ccdd38e7e5573ca35db6ca
phlgr <philipp@gartz.dev>
2021-05-31 16:10:50 +0200

    ✨ Add React Router to Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import { addDecorator } from '@storybook/react';
+import { MemoryRouter } from 'react-router-dom';
+
 import '../src/app/globals.css';
 
 export const parameters = {
@@ -9,3 +12,5 @@ export const parameters = {
     },
   },
 };
+
+addDecorator((story) => <MemoryRouter>{story()}</MemoryRouter>);
