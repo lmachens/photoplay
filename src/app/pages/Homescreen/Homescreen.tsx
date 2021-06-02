1cb40e0c6425046efcf2de88c78e0b57c22e452d
Dev-dfm <d.floresmedina@gmail.com>
2021-06-02 11:26:35 +0200

    ♻️ Remove useEffect ans useState import

--- src/app/pages/Homescreen/Homescreen.tsx
+++ src/app/pages/Homescreen/Homescreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import NavigationGenre from '../../components/NavigationGenre/NavigationGenre';
 import Rating from '../../components/Rating/Rating';
 import MovieCard from '../../components/MovieCard/MovieCard';
