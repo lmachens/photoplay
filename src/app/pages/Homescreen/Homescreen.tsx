8a0321ed11e5bad948584f2b5af42734c9d20180
Dev-dfm <d.floresmedina@gmail.com>
2021-06-02 09:57:55 +0200

    🎨  Add useFetch hook

--- src/app/pages/Homescreen/Homescreen.tsx
+++ src/app/pages/Homescreen/Homescreen.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavigationGenre from '../../components/NavigationGenre/NavigationGenre';
 import Rating from '../../components/Rating/Rating';
 import MovieCard from '../../components/MovieCard/MovieCard';
 import styles from './Homescreen.module.css';
 import NavBar from '../../components/NavBar/NavBar';
+import { PopularMovie } from '../../../types';
 
 const categoriesArray = ['Movie', 'Adventure', 'Comedy', 'Family'];
 
 function Homescreen(): JSX.Element {
+  const [popularMovies, setPopularMovies] = useState<PopularMovie[] | null>(
+    null
+  );
+
+  useEffect(() => {
+    fetch('api/movies/popular')
+      .then((response) => response.json())
+      .then(setPopularMovies);
+  });
+
+  if (!popularMovies) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <main className={styles.container}>
-      <img className={styles.poster} src="./dora.png" />
+      <img className={styles.poster} src={popularMovies[0].posterPath || ''} />
       <Rating value={4}></Rating>
       <div className={styles.navigationGenre}>
         <NavigationGenre categories={categoriesArray}></NavigationGenre>
