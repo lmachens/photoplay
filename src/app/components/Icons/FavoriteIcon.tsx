0e4b38bc3005bef8295b7c28fdfc6ca6e8e5cc77
jujuzitroni <jujuzitroni@gmail.com>
2021-06-03 14:08:48 +0200

    💄 Implement and style favorite icon in NavBar

--- src/app/components/Icons/FavoriteIcon.tsx
+++ src/app/components/Icons/FavoriteIcon.tsx
@@ -2,27 +2,18 @@ import React from 'react';
 
 export default function FavoriteIcon(): JSX.Element {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="2 0 25 25"
+      width="20"
+      height="20"
+    >
       <polygon
-        fill="none"
-        stroke="#000"
+        fill="currentColor"
         stroke-linecap="round"
         stroke-linejoin="round"
-        stroke-width="2"
         points="15 5 18 13 26 13 19 18 22 26 15 21 8 26 11 18 4 13 12 13 15 5"
       />
-      <defs>
-        <linearGradient
-          id="linear-gradient"
-          x1="0.5"
-          x2="0.5"
-          y2="1"
-          gradientUnits="objectBoundingBox"
-        >
-          <stop offset="0" stop-color={'var(--primary-gradient-from)'} />
-          <stop offset="1" stop-color={'var(--primary-gradient-to)'} />
-        </linearGradient>
-      </defs>
     </svg>
   );
 }
