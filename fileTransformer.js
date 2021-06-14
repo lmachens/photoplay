d3fd711e884d4daf5eda74ba658d221d871472ed
lmachens <leon@machens.koeln>
2021-06-14 15:19:16 +0200

    🔧 Configure JEST to support React, TypeScript and asset imports

---
+++ fileTransformer.js
@@ -0,0 +1,9 @@
+// fileTransformer.js
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+module.exports = {
+  process(src, filename) {
+    return 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';';
+  },
+};
