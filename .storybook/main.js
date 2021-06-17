30845a6cfa3ede7eb9a9868c91928875aa5d4513
lmachens <leon@machens.koeln>
2021-06-17 17:50:28 +0200

    🐛 Support deployment on /storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,4 +4,9 @@ module.exports = {
   core: {
     builder: 'storybook-builder-vite',
   },
+  async viteFinal(config) {
+    // Set relative base path to support deployment on path like /storybook
+    config.base = './';
+    return config;
+  },
 };
