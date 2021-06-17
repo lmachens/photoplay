731cb6383019412739fb3c0ce009e9e414f7fe65
lmachens <leon@machens.koeln>
2021-06-17 17:48:32 +0200

    🐛 Import React

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { addDecorator } from '@storybook/react';
 import { MemoryRouter } from 'react-router-dom';
 import MobileLayout from '../src/app/components/MobileLayout/MobileLayout';
